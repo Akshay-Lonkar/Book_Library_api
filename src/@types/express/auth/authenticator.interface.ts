a135d92bdf0b479ec2865f62bd50d183fe598f26
Akshay-Lonkar <82947115+Akshay-Lonkar@users.noreply.github.com>
2022-03-11 17:40:02 +0530

    Delete src directory

--- src/@types/express/auth/authenticator.interface.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { AuthenticationResult } from '../domain.types/auth/auth.domain.types';
-
-export interface IAuthenticator {
-    authenticateUser(request: express.Request): Promise<AuthenticationResult>;
-}
